06ffdb2b4fdbed7cac12772ec589f60f573c5a6b
Alexander Gruzberg <sasha@gruzberg.ru>
2020-12-25 07:53:06 +0300

    addition

---
+++ labs/lab_5.jl
@@ -0,0 +1,35 @@
+"""
+ДАНО: Робот - в произвольной клетке ограниченного прямоугольного поля, на котором могут находиться также внутренние прямоугольные перегородки (все перегородки изолированы друг от друга, прямоугольники могут вырождаться в отрезки)
+РЕЗУЛЬТАТ: Робот - в исходном положении и в углах поля стоят маркеры
+"""
+function mark_4(r::Robot)
+    steps=0
+    arr_of_steps=[] #Массив
+    while ((isborder(r,Sud))&&(isborder(r,West)))==false
+        push!(arr_of_steps,moves!(r,West)) #Добавление элемента в конец массива
+        push!(arr_of_steps,moves!(r,Sud))
+        steps=steps+2
+    end
+    for side in (Nord,Ost,Sud,West)
+        moves!(r,side)
+        putmarker!(r)
+    end
+    while (steps>0)==true
+        side=isodd(steps) ? Ost : Nord #Тернарный оператор
+        for a in 1:arr_of_steps[steps]
+            move!(r,side)
+        end
+        steps=steps-1
+    end
+end
+
+function moves!(r::Robot,side::HorizonSide)
+    num_steps=0
+    while isborder(r,side)==false
+        move!(r,side)
+        num_steps=num_steps+1
+    end
+    return num_steps
+end
+
+
